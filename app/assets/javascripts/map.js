59db8532b676511c0e9b283c844f0658d8c7aa52
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-11-19 16:05:39 +0900

    id付与

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,30 +1,40 @@
 $(function() {
   $("html").keyup(function(e){
-    console.log("ye")
     switch(e.which){
         case 39: // Key[→]
-        $('.icon').animate({left: '+=25px'},100);
+        $('.icon').animate({left: '+=50px'},100);
+        iia = $(".icon").attr('id')
+        let iiia = Number(iia) + 1
+        $(".icon").attr("id",iiia)
         break;
 
         case 37: // Key[←]
-        $('.icon').animate({left: '-=25px'},100);
+        $('.icon').animate({left: '-=50px'},100);
+        iib = $(".icon").attr('id')
+        let iiib = Number(iib) - 1
+        $(".icon").attr("id",iiib)    
         break;
 
         case 38: // Key[↑]
-        $('.icon').animate({top: '-=25px'},100);
+        $('.icon').animate({top: '-=50px'},100);
+        iic = $(".icon").attr('id')
+        let iiic = Number(iic) - 20
+        $(".icon").attr("id",iiic)        
         break;
 
         case 40: // Key[↓]
-        $('.icon').animate({top: '+=25px'},100);
+        $('.icon').animate({top: '+=50px'},100);
+        iid = $(".icon").attr('id')
+        let iiid = Number(iid) + 20
+        $(".icon").attr("id",iiid)        
         break;
     }
   });
   function buildHTML(i) {
-    let html = `<div class="squares id= "${i}"></div>`
+    let html = `<div id = "${i}" class="squares"></div>`
     $('.wrapper').append(html)
   }
-  for (var i = 0; i < 320 ; i++) {
+  for (var i = 0; i < 300 ; i++) {
     buildHTML(i)
-    
 }
 });
